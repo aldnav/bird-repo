e51e61083935f8f3f75721e1d77c1d4c0c4ca5f4
Aldrin Navarro <aldrinnavarro16@gmail.com>
2016-09-16 10:42:11 +0800

    Remove unnecessary use of glob

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var path = require('path');
-var glob = require('glob');
 var library = require('./library.js');
 
 
@@ -18,14 +17,6 @@ server.listen(port, function() {
     console.log('Listening on port ' + port + '...');
 });
 
-// find available images for background
-// var bg;
-// glob(__dirname + '/public/images/**/*.*',
-//     {cache: 'FILE'}, function(er, files) {
-//     bg = files[Math.floor(Math.random() * files.length)];
-//     bg = bg.split('/').pop();
-// });
-
 io.on('connection', function (socket) {
     socket.emit('h4ndsh4k3', {message: 'ACK RCVD'});
     socket.on('search', function (data) {
@@ -38,11 +29,3 @@ io.on('connection', function (socket) {
         });
     });
 });
-
-// library.libSearch('King Penguin', function(results) {
-//     console.log(results.results.content);
-// });
-//
-// library.wikiSearch('King Penguin', function(results) {
-//     console.log(results);
-// });
